5ffbe0b04c644f01679f39163be36c8bcd1f2f67
Arturmatossimoes <142237812+Arturmatossimoes@users.noreply.github.com>
2025-07-16 09:53:41 -0300

    dados_reais.py

---
+++ dados_reais.py
@@ -0,0 +1,7 @@
+dados_setores = [
+    {"setor": "TI", "burnout": 42.5, "sintomas": 38.1},
+    {"setor": "Administração", "burnout": 38.1, "sintomas": 40.7},
+    {"setor": "Marketing", "burnout": 37.9, "sintomas": 39.8},
+    {"setor": "Produção", "burnout": 34.6, "sintomas": 43.2},
+    {"setor": "Atendimento", "burnout": 31.4, "sintomas": 44.2}
+]
